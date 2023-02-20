46c1a1fb7d2b17866840be63aa50a43c6d9005ae
Aiden <adisihan2021@gmail.com>
2023-02-20 14:13:20 -0500

    Initial commit

---
+++ app/src/main/java/com/adsh/material/MainActivity.java
@@ -0,0 +1,26 @@
+package com.adsh.material;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton floatingActionButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "fab", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
